d818901cc7af6179a8830f95aa15129e7687d8c6
UlisesMC29 <contac_umc@hotmail.com>
2022-05-26 18:02:18 -0500

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/02-QR-code/', //This is the base url of your website 
   plugins: [react()]
 })
